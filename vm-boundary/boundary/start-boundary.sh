ae375c3bf37b9b0fb88b58618e68c18707ec8c0a
joshuakimrub <joshuakim220497@gmail.com>
2024-12-08 15:46:04 +0100

    correct replace of variables

--- vm-boundary/boundary/start-boundary.sh
+++ vm-boundary/boundary/start-boundary.sh
@@ -8,14 +8,14 @@ POSTGRES_PASSWORD=$(az keyvault secret show --vault-name task-vault --name Bound
 CERT_PATH=${CERT_FOLDER}/${SERVER_NAME}.crt
 KEY_PATH=${CERT_FOLDER}/${SERVER_NAME}.key
 
-sed -i "s/\${CERT}/$CERT_PATH/g" boundary.hcl
-sed -i "s/\${KEY}/$KEY_PATH/g" boundary.hcl
-sed -i "s/\${POSTGRES_PASSWORD}/$POSTGRES_PASSWORD/g" boundary.hcl
-sed -i "s/\${AES_KEY}/$AES_KEY/g" boundary.hcl
+sed -i "s|\${CERT}|$CERT_PATH|g" boundary.hcl
+sed -i "s|\${KEY}|$KEY_PATH|g" boundary.hcl
+sed -i "s|\${POSTGRES_PASSWORD}|$POSTGRES_PASSWORD|g" boundary.hcl
+sed -i "s|\${AES_KEY}|$AES_KEY|g" boundary.hcl
 
 boundary server -config boundary.hcl
 
-sed -i "s/$CERT_PATH/\${CERT}/g" boundary.hcl
-sed -i "s/$KEY_PATH/\${KEY}/g" boundary.hcl
-sed -i "s/$POSTGRES_PASSWORD/\${POSTGRES_PASSWORD}/g" boundary.hcl
-sed -i "s/$AES_KEY/\${AES_KEY}/g" boundary.hcl
+sed -i "s|$CERT_PATH|\${CERT}|g" boundary.hcl
+sed -i "s|$KEY_PATH|\${KEY}|g" boundary.hcl
+sed -i "s|$POSTGRES_PASSWORD|\${POSTGRES_PASSWORD}|g" boundary.hcl
+sed -i "s|$AES_KEY|\${AES_KEY}|g" boundary.hcl
