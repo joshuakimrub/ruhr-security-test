12c1f420236e7adafa634c506ccee9f712873d9d
joshuakimrub <joshuakim220497@gmail.com>
2024-12-08 15:23:52 +0100

    removed export

--- deployment/variables.sh
+++ deployment/variables.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Service principal shall be created manually
-export CLIENT_SECRET=$(az keyvault secret show --vault-name task-vault --name ClientSecret --query value -o tsv)
-export CLIENT_ID='7186b5c0-9e6a-4637-89f0-2c67144ebe20'
-export TENANT_ID='dc90e60f-fd1e-40a6-81c5-903cb3cc6a92'
-export SUBSCRIPTION_ID='eda1bd7d-0cd7-41eb-92dd-aa84171f2820'
-export RESOURCE_GROUP="ruhr-security-task"
-export VM_NAME="hardened-vm"
-export IMAGE="hardened-ubuntu-2204-no-azure"
-export ADMIN_NAME="azadmin"
-export SHH_PUB_KEY="/home/kim/.ssh/id_ed25519.pub"
-export LOCATION="germanywestcentral"
-export 
-export CA_NAME="JK-CA"
-export SERVER_NAME="VM"
-export CERT_FOLDER=/etc/certs/
+CLIENT_SECRET=$(az keyvault secret show --vault-name task-vault --name ClientSecret --query value -o tsv)
+CLIENT_ID='7186b5c0-9e6a-4637-89f0-2c67144ebe20'
+TENANT_ID='dc90e60f-fd1e-40a6-81c5-903cb3cc6a92'
+SUBSCRIPTION_ID='eda1bd7d-0cd7-41eb-92dd-aa84171f2820'
+RESOURCE_GROUP="ruhr-security-task"
+VM_NAME="hardened-vm"
+IMAGE="hardened-ubuntu-2204-no-azure"
+ADMIN_NAME="azadmin"
+SHH_PUB_KEY="/home/kim/.ssh/id_ed25519.pub"
+LOCATION="germanywestcentral"
+
+CA_NAME="JK-CA"
+SERVER_NAME="VM"
+CERT_FOLDER=/etc/certs/
--- vm-boundary/boundary/start-boundary.sh
+++ vm-boundary/boundary/start-boundary.sh
@@ -2,7 +2,7 @@
 
 source ../../deployment/variables.sh
 
-export AES_KEY=$(az keyvault secret show --vault-name task-vault --name KmsAesKey --query value -o tsv)
-export POSTGRES_PASSWORD=$(az keyvault secret show --vault-name task-vault --name BoundaryPostgresPassword --query value -o tsv)
+AES_KEY=$(az keyvault secret show --vault-name task-vault --name KmsAesKey --query value -o tsv)
+POSTGRES_PASSWORD=$(az keyvault secret show --vault-name task-vault --name BoundaryPostgresPassword --query value -o tsv)
 
 boundary server -config boundary.hcl
